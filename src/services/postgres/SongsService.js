c49a3ad0130050987f46b43e1113702e44817772
Burhanudin Yahya <bor.yahya@gmail.com>
2022-10-14 06:46:36 +0700

    use rowCount

--- src/services/postgres/SongsService.js
+++ src/services/postgres/SongsService.js
@@ -46,7 +46,7 @@ class SongsService {
     };
     const result = await this._pool.query(query);
 
-    if (!result.rows.length) {
+    if (!result.rowCount) {
       throw new NotFoundError('Song not found!');
     }
 
